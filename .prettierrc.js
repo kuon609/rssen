cc09523c306372cc9efb2650592a788def9bd196
kuon609 <kuon@prog-8.com>
2022-12-04 20:35:04 +0900

    format

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: false,
+  arrowParens: 'avoid',
+  // @trivago/prettier-plugin-sort-imports
+  // importOrder: ["<THIRD_PARTY_MODULES>", "^path-api-proto/(.*)$", "^@/(.*)$"],
+  // importOrderSeparation: true,
+  // importOrderCaseInsensitive: true,
+}
