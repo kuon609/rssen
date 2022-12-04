159b38656430e45648c358aa5032e4499eade955
kuon609 <kuon@prog-8.com>
2022-12-04 20:48:24 +0900

    add firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDfTMaBKVsGlPLLrWqYvzyWkqBlt3qSxn8',
+  authDomain: 'osewa-77670.firebaseapp.com',
+  projectId: 'osewa-77670',
+  storageBucket: 'osewa-77670.appspot.com',
+  messagingSenderId: '102086719204',
+  appId: '1:102086719204:web:c53dd823bc9e63b6225ef1',
+  measurementId: 'G-VYD5TTBNB4',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
