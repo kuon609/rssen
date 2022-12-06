73df193ba03675140a075251c891879e6553e229
kuon609 <kuon@prog-8.com>
2022-12-06 16:36:13 +0900

    add login

--- components/Login.tsx
+++ components/Login.tsx
@@ -3,26 +3,26 @@ import {StyleSheet, Text, View} from 'react-native'
 import {getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged} from 'firebase/auth'
 import {useEffect, useState} from 'react'
 import {app} from '@/firebaseConfig'
-export default function App() {
-  const auth = getAuth(app)
-  const [data, setData] = useState({email: '', password: ''})
-  const handleInputs = (e: {target: HTMLInputElement}) => {
-    const input = {[e.target.name]: e.target.value}
-    setData({...data, ...input})
-  }
-  const addData = () => {
-    signInWithEmailAndPassword(auth, data.email, data.password)
-  }
-  const handleLogout = () => signOut(auth)
-  useEffect(() => {
-    onAuthStateChanged(auth, user => {
-      if (user) {
-        alert('user is logged in')
-      } else {
-        alert('user is logged out')
-      }
-    })
-  }, [])
+const auth = getAuth(app)
+const [data, setData] = useState({email: '', password: ''})
+const handleInputs = (e: {target: HTMLInputElement}) => {
+  const input = {[e.target.name]: e.target.value}
+  setData({...data, ...input})
+}
+const addData = () => {
+  signInWithEmailAndPassword(auth, data.email, data.password)
+}
+const handleLogout = () => signOut(auth)
+useEffect(() => {
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      alert('user is logged in')
+    } else {
+      alert('user is logged out')
+    }
+  })
+}, [])
+export const Login = () => {
   return (
     <View style={styles.container}>
       <input type='email' name='email' placeholder='Email' onChange={e => handleInputs(e)} />
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
 import {initializeApp} from 'firebase/app'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyDfTMaBKVsGlPLLrWqYvzyWkqBlt3qSxn8',
   authDomain: 'osewa-77670.firebaseapp.com',
@@ -15,5 +10,4 @@ const firebaseConfig = {
   measurementId: 'G-VYD5TTBNB4',
 }
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
+export const app = initializeApp(firebaseConfig)
